c924d484a6d8ac1cd0db4f1315236a677aaee9fe
Fb <brunnockf@gmail.com>
2022-09-30 13:44:39 -0500

    create playRound function and start game function

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-function computerSelection () {
+function getComputerChoice () {
     var randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0){
-        return "Rock";
+        return "rock";
     } else if (randomNumber === 1) {
-        return "Paper";
+        return "paper";
     } else if (randomNumber === 2) {
-        return "Scissors"
+        return "scissors";
     };
 };
+
+function playRound (playerSelection, computerSelection) {
+    var playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    if (playerSelection === computerSelection) {
+        return "Draw";
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        return "You lose!";
+    } else {
+        return "You win!";
+    };
+}
+
+function game () {
+    var result = playRound(prompt("Input your choice"), getComputerChoice());
+    console.log(result);
+}
